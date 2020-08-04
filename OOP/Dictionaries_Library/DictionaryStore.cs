89044d6aed64323928f379df9e3a68e8c6f4e3ff
Nikolaj Hoggins <nikolajhjacobsen@gmail.com>
2020-08-04 09:04:53 +0200

    Created two dictionaries and a testing software to test library

---
+++ OOP/Dictionaries_Library/DictionaryStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries_Library
+{
+    public class DictionaryStore
+    {
+        //This dictionary will return the interger value of a give number in string form (from 1-10)
+        public Dictionary<string, int> numbers = new Dictionary<string, int>();
+        
+        //This dictionary will retun if a number is cool or not.
+        public Dictionary<float, bool> isNice = new Dictionary<float, bool>();
+    }
+}
