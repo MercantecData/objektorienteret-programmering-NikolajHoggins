d76cdf345b8e771424cedadb4aa299d2c0182be1
Nikolaj Hoggins <nikolajhjacobsen@gmail.com>
2020-08-05 11:57:17 +0200

    Added overloading Math library

---
+++ OOP/Overloading_Library/Math.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Overloading_Library
+{
+    public class MathClass
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        public static int Add(string a, string b)
+        {
+            return parseNum(a) + parseNum(b);
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+        public static int Subtract(string a, string b)
+        {
+            return parseNum(a) - parseNum(b);
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        public static int Multiply(string a, string b)
+        {
+            return parseNum(a) * parseNum(b);
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static float Divide(float a, float b)
+        {
+            return a / b;
+        }
+        public static int Divide(string a, string b)
+        {
+            return parseNum(a) / parseNum(b);
+        }
+        
+        private static int parseNum(string num)
+        {
+            bool isParsable = Int32.TryParse(num, out int number);
+
+            if (isParsable)
+                return(number);
+            else
+            {
+                Console.WriteLine("[Error] Could not parse string to number.");
+                return -1;
+            }
+        }
+    }
+}
---
+++ OOP/Overloading_Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Overloading_Library;
+
+namespace Overloading_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathClass.Add(2, 2));
+            Console.WriteLine(MathClass.Add(2.0f, 2.0f));
+            Console.WriteLine(MathClass.Add("2", "2"));
+
+            Console.WriteLine(MathClass.Divide("24", "9"));
+        }
+    }
+}
