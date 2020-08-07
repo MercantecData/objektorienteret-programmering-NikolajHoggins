30c42366482087aa75b56e70cceabcf0713a12ab
Nikolaj Hoggins <nikolajhjacobsen@gmail.com>
2020-08-07 10:42:25 +0200

    Delegates standart tasks.

---
+++ OOP/Delegates_Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Delegates_Library;
+namespace Delegates_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateContainer.del();
+            Console.WriteLine(DelegateContainer.floatDel());
+            DelegateContainer.intakeDel("yeetus", "deletus", "the fetus");
+
+        }
+    }
+}
