816ae617fbb8648a5ee2e47e0a834f3561c2f5f5
Nikolaj Hoggins <nikolajhjacobsen@gmail.com>
2020-08-07 11:00:00 +0200

    Delegates bonus task

--- OOP/Delegates_Test/Program.cs
+++ OOP/Delegates_Test/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using Delegates_Library;
+using Overloading_Library;
+    
 namespace Delegates_Test
 {
     class Program
     {
+        public delegate int AddDel(int a, int b);
+        public delegate float AddFloatDel(float a, float b);
+
         static void Main(string[] args)
         {
             DelegateContainer.del();
             Console.WriteLine(DelegateContainer.floatDel());
             DelegateContainer.intakeDel("yeetus", "deletus", "the fetus");
 
+            
+            
+            
+            //Bonus task:
+            AddDel addDel = MathClass.Add;
+            AddFloatDel addFloatDel = MathClass.Add;
+            
+            Console.WriteLine(addDel(1, 4));
+            Console.WriteLine(addFloatDel(1.5f, 4.2f));
+            
+
         }
+        
+        
     }
 }
